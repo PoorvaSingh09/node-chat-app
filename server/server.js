9b4dbd8879cf9d41ac940339635719ba4589541e
PoorvaSingh <poorva.singh@nagarro.com>
2018-04-25 22:03:51 +0530

    Send current location to user and open map with latitude and longitude

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const port = process.env.PORT || 3000;
 
@@ -26,20 +26,16 @@ io.on('connection', (socket) => {
     console.log('Message from client:', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from server');
+  });
 
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', (socket) => {
     console.log('User disconnected');
   });
 });
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,6 +10,19 @@ describe('generateMessage', () => {
     expect(res.from).toBe(from);
     expect(res.text).toBe(text);
     expect(res).toInclude({from, text});
+    expect(res.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Admin';
+    var latitude = 28.9;
+    var longitude = 11.1;
+
+    var res = generateLocationMessage(from, latitude, longitude);
+    var url = 'https://www.google.com/maps?q=28.9,11.1';
+    expect(res).toInclude({from, url});
     expect(res.createdAt).toBeA('number');
   });
 });
